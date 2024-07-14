e238344897f0369bcaf6a6bd831dbcb85d4c04d3
J0SAL <joyalmeida321@gmail.com>
2024-07-14 09:35:20 +0530

    auth

---
+++ backend/decorators.py
@@ -0,0 +1,27 @@
+from functools import wraps
+from flask import request, abort
+from firebase_admin import auth
+
+def verify_token(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if request.method == 'OPTIONS':
+            return f(*args, **kwargs)
+        
+        auth_header = request.headers.get("X-Firebase-AppCheck")
+        
+        if auth_header is None:
+            abort(401)
+        
+        try:
+            app_check_token = auth_header
+            # Verify ID token
+            app_check_claims = auth.verify_id_token(app_check_token)
+            # print(f"Verified claims: {app_check_claims}")
+            # Pass the claims to the route
+            return f(*args, **kwargs, claims=app_check_claims)
+        except Exception as e:
+            print(f"Error: {str(e)}")
+            abort(401)
+    
+    return decorated_function
