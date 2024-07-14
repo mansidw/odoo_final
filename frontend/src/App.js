a64e6e02d7ba7ae2e7435f2cbfb198353e52946a
Gaurav Yashwant Parulekar <gauravparulekar02@gmail.com>
2024-07-14 12:21:53 +0530

    Appjs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import TopNav from "./Components/TopNavbar/TopNav";
 import Profile from "./Components/Profile/Profile";
+import Explore from "./Components/Explore/Explore";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/explore" element={<Explore />} />
       </Routes>
     </div>
   );
