9107b2fb2d135a9276f1c198c4d9ece0760187c2
Gaurav Yashwant Parulekar <gauravparulekar02@gmail.com>
2024-07-14 09:19:40 +0530

    Basic setup and topnav

---
+++ frontend/src/Components/TopNavbar/TopNav.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import "./TopNav.css";
+import { Link } from "react-router-dom";
+
+const TopNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="topnav_container">
+      <div className="topnav_main_element">
+        <div className="topnav_ele logo">
+          <Link to="/">
+            <img className="navbar_logo" src="./logo.png" alt="" />
+          </Link>
+        </div>
+        <div className={`topnav_ele menu ${isOpen ? "open" : ""}`}>
+          <Link to="/explore">
+            <p className="topnav_menu_item">Explore</p>
+          </Link>
+          <Link to="/history">
+            <p className="topnav_menu_item">History</p>
+          </Link>
+          <Link to="/profile">
+            <p className="topnav_menu_item">Profile</p>
+          </Link>
+        </div>
+        <div className={`topnav_ele buttons ${isOpen ? "open" : ""}`}>
+          <button className="top_nav_button">Sign In</button>
+          <button className="top_nav_button">Sign Up</button>
+        </div>
+        <div className="topnav_ele hamburger" onClick={toggleMenu}>
+          <div className="hamburger_icon">&#9776;</div>
+        </div>
+      </div>
+      {isOpen && (
+        <div className="dropdown_menu">
+          <Link to="/explore" onClick={toggleMenu}>
+            <p className="topnav_menu_item">Explore</p>
+          </Link>
+          <Link to="/history" onClick={toggleMenu}>
+            <p className="topnav_menu_item">History</p>
+          </Link>
+          <Link to="/profile" onClick={toggleMenu}>
+            <p className="topnav_menu_item">Profile</p>
+          </Link>
+          <div className="topnav_collapse_button">
+            <button className="top_nav_button" onClick={toggleMenu}>
+              Sign In
+            </button>
+            <button className="top_nav_button" onClick={toggleMenu}>
+              Sign Up
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TopNav;
