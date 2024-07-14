14ce859ace20f4583b794b2aaa3ddc6b8192de9c
Pranjal Patil <pranjalpatil@Pranjals-MacBook-Air.local>
2024-07-14 12:56:03 +0530

    2 backend api

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from flask_cors import CORS, cross_origin
 import flask
 from uuid import uuid4
+from datetime import datetime
 
 from decorators import verify_token
 
@@ -17,10 +18,6 @@ default_app = initialize_app(cred)
 db = firestore.client()
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
-
 @app.route('/check-auth', methods=['GET','POST'])
 @verify_token
 def checkauth(claims):
@@ -28,6 +25,37 @@ def checkauth(claims):
         return {"res":" working"}, 200
     except Exception as ex:
         return {"error": str(ex)}, 500
+    
+@app.route('/new_arrivals', methods=['GET'])
+def new_arrival_books():
+    try:
+        today_date = datetime.now().strftime("%Y-%m-%d")
+        
+        # Query books collection where timestamp equals today's date
+        books_ref = db.collection('books').where('timestamp', '==', today_date).limit(5).get()
+        
+        books = []
+        for doc in books_ref:
+            books.append(doc.to_dict())
+        
+        return jsonify({"success": True, "books": books}), 200
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+
+@app.route('/most_trending', methods=['GET'])
+def most_trending_books():
+    try:
+        books_ref = db.collection('books').order_by('borrow_count', direction=firestore.Query.DESCENDING).limit(10).get()
+        books = []
+        for doc in books_ref:
+            books.append(doc.to_dict())
+        
+        return jsonify(books), 200
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
