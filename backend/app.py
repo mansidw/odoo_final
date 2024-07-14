7d8f6d1c4d0edde521584de8149ef5e100b5a5c4
Mansi Dwivedi <68280369+mansidw@users.noreply.github.com>
2024-07-14 08:09:59 +0530

    initialized frontend and backend

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+
+if __name__ == '__main__':
+    app.run()
