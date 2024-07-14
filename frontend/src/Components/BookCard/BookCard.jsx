f00e0a1845549a0e79e6e1ede4f42034f69a971c
Gaurav Yashwant Parulekar <gauravparulekar02@gmail.com>
2024-07-14 10:30:54 +0530

    Responsive book card

--- frontend/src/Components/BookCard/BookCard.jsx
+++ frontend/src/Components/BookCard/BookCard.jsx
@@ -11,9 +11,8 @@ const BookCard = () => {
         />
       </div>
       <div className="book_card_item">
-        <h4>Odoo 10 Implementation Cookbook</h4>
-        <p>Published Date: 2017-10-06</p>
-        <p>Author: Mantavy Gajjar</p>
+        <h4 className="blue_text">Odoo 10 Implementation Cookbook</h4>
+        <p className="blue_text">Mantavy Gajjar | 2017-10-06</p>
         <p>
           Comprehensive tasks covering Odoo 10 in the right wayAbout This Book*
           Reduce implementation costs and improve major benchmarks relating to
